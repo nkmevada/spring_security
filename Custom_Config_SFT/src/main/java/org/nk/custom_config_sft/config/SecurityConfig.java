0c9d19840d7aead0d1ada2ddf3929f01871a695d
Niki Mevada <nkmevada2801@gmail.com>
2025-08-13 11:17:22 -0400

    beautify the code

--- Custom_Config_SFT/src/main/java/org/nk/custom_config_sft/config/SecurityConfig.java
+++ Custom_Config_SFT/src/main/java/org/nk/custom_config_sft/config/SecurityConfig.java
@@ -14,11 +14,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(customizer -> customizer.disable());
-        http.authorizeHttpRequests(request-> request.anyRequest().authenticated());
-        http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
-        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        return http.build();
+      return http.csrf(customizer -> customizer.disable())
+        .authorizeHttpRequests(request-> request.anyRequest().authenticated())
+        .formLogin(Customizer.withDefaults())
+        .httpBasic(Customizer.withDefaults())
+        .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .build();
     }
 }
