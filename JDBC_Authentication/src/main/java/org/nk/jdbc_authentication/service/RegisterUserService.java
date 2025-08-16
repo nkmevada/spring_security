82bec5f4a4ed0d564126a743f1093920945a4c4d
Niki Mevada <nkmevada2801@gmail.com>
2025-08-16 17:13:18 -0400

    implement BCryptPasswordEncoder functionality

--- JDBC_Authentication/src/main/java/org/nk/jdbc_authentication/service/RegisterUserService.java
+++ JDBC_Authentication/src/main/java/org/nk/jdbc_authentication/service/RegisterUserService.java
@@ -3,6 +3,7 @@ package org.nk.jdbc_authentication.service;
 import org.nk.jdbc_authentication.model.Users;
 import org.nk.jdbc_authentication.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,7 +13,10 @@ public class RegisterUserService {
     @Autowired
     UserRepo userRepo;
 
+    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
     public void save(Users users) {
+        users.setPassword(bCryptPasswordEncoder.encode(users.getPassword()));
         userRepo.save(users);
     }
 
